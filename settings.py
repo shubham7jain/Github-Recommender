6a78466e01f13176978b5503f442407d23121669
Shubham Jain <shubham7jain@gmail.com>
2017-04-04 01:11:22 -0500

    Adding initial recommender based on starred repositories of the user

---
+++ settings.py
@@ -0,0 +1,12 @@
+'settings'
+
+# recommendations for...
+user = 'shubham7jain'
+
+# github username and password
+auth = ( 'shubham7jain', 'PASSWORD' )
+
+# where to store data
+# no slashes at the end
+users_dir = 'users'		
+repos_dir = 'repos'
